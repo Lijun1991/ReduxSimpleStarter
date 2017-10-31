15ce68971967d142635f65692db39e8f5f993925
Wang Lijun <wang283808065@gmail.com>
2017-10-31 11:23:32 -0700

    youtude video browser

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
+import _ from "lodash";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-const API_KEY = 'AIzaSyBu3-8E0_edrgjswdl-gQ_0-SEUa40PKMo';
+const API_KEY = 'AIzaSyA2fl7cOecdvjPrgpTGg0UjtPLaQCgi-L8';
 
 class App extends Component {
     constructor(props) {
-        super(props);
-
-        this.state = ({ videos: [] });
-
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+      super(props);
+  
+      this.state = {
+        videos: [],
+        selectedVideo: null
+      };
+  
+      this.videoSearch("42 usa");
+    }
+  
+    videoSearch(term) {
+      YTSearch({ key: API_KEY, term: term }, videos => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
         });
+      });
     }
-    render () {
-        return (
-            <div>
-                <SearchBar />
-            </div>
-        );
+  
+    render() {
+      const videoSearch = _.debounce(term => {
+        this.videoSearch(term);
+      }, 300);
+  
+      return (
+        <div>
+          <SearchBar onSearchTermChange={videoSearch} />
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList
+            onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+            videos={this.state.videos}
+          />
+        </div>
+      );
     }
-}
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+  }
+  
+  ReactDOM.render(<App />, document.querySelector(".container"));
+  
